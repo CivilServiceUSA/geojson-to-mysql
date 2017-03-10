40fcd5032d51922e5caaa471e9897c247cf00a25
John Kramlich <me@johnkramlich.com>
2017-03-09 20:11:54 -0600

    Accept table, column names, directory to import.  Std output is MySQL queries to execute.  --insert flag allows direct import into MySQL

--- import.php
+++ import.php
@@ -3,42 +3,61 @@
  * GeoJSON to MySQL importer
  *
  * @note this is quick and dirty.  It's more of a proof of concept because it's specific to a single table type and connection parameters are hardcoded.
+ *
+ * @usage php import.php --table district_shapes --keys state_code,district --dir /home/tmp/districts --insert
  */
 
-$host = "localhost";
-$username = 'replace';
-$password = 'replace';
-$dbname = 'replace';
+$config = parse_ini_file('./config.ini');
+$options = getopt("", array('table:', 'keys:', 'dir:', 'insert'));
 
-$importPath = 'path_to_geo_json';
+$table   = $options['table'];
+$keys    = $options['keys'];
+$dir    = $options['dir'];
+$insertIntoDb = (isset($options['insert'])) ? true : false;
 
-$mysqli = new mysqli($host, $username, $password, $dbname);
-$query = "INSERT INTO district_shapes (`state`, `district_id`, `shape`) VALUES (? , ? , ST_GeomFromGeoJSON(?))";
+$importPath = $dir;
 
-echo "Importing...", PHP_EOL;
+$keysToImport = explode(',', $keys);
 
+$columnTemplate = array();
 
-if ($stmt = $mysqli->prepare($query)) {
-
-	foreach (glob("{$importPath}/*.geojson") as $filePath){
-
-		$geojson = file_get_contents($filePath);
-
-		$json = json_decode($geojson, true);
-
-		$stateCode = $json['properties']['state_code'];
-		$districtId = $json['properties']['district'];
-
-
-		echo $filePath, PHP_EOL;
-
-		$stmt->bind_param("sss", $stateCode, $districtId, $geojson);
-		$stmt->execute();
-
-	}
+foreach($keysToImport as $keyName){
+	$columnTemplate[$keyName] = '';
 }
 
 
+// Must create a connection to MySQL so real_escape_string works
+// @todo don't rely on MySQL connection
+$mysqli = new mysqli($config['host'], $config['username'], $config['password'], $config['dbname']);
 
 
+$query = "INSERT INTO district_shapes (`state`, `district_id`, `shape`) VALUES (? , ? , ST_GeomFromGeoJSON(?))";
 
+
+foreach (glob("{$importPath}/*.geojson") as $filePath){
+
+	$geojson = file_get_contents($filePath);
+	$json = json_decode($geojson, true);
+
+	$rowValues = $columnTemplate;
+
+	foreach($keysToImport as $keyName){
+		$rowValues[$keyName] = (isset($json['properties'][$keyName])) ? $json['properties'][$keyName] : '';
+	}
+
+	$setString = ' ';
+
+	foreach($rowValues as $keyName => $value){
+		$setString .= "`{$keyName}` = '".$mysqli->real_escape_string($value)."', ";
+	}
+
+	$setString .= " `shape` = ST_GeomFromGeoJSON('".$mysqli->real_escape_string($geojson)."')";
+
+	$query = "INSERT INTO `{$table}` SET {$setString} ; \r\n\r\n";
+
+	if($insertIntoDb){
+		$mysqli->query($query);
+	} else {
+		echo $query;
+	}
+}
